c3aee8fa6fa50185aff97b4bc4b2a8967efffd0f
MariaLuizaBarbosa <malubarosa2708@gmail.com>
2024-03-08 10:27:47 -0300

    exercicio 3

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,10 @@ Route::get('nome/idade', function (Request $request) {
     $idade = '16';
     return "Meu nome é " . $nome . ", tenho" . $idade . " anos de idade";
 });
+
+Route::get('nome/cidade', function (Request $request) {
+    $nome = "Malu";
+    $ano = '2007';
+    $cidade = "Presidente Epitacio.";
+    return "Meu nome é " . $nome . ", nasci no ano de " . $ano . " na cidade de " . $cidade;
+});
