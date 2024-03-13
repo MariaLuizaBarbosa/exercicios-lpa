98ab54a7f891c37df1b7088b9776a2f191470441
MariaLuizaBarbosa <malubarosa2708@gmail.com>
2024-03-13 15:28:19 -0300

    criei 17 exercicios

--- routes/api.php
+++ routes/api.php
@@ -4,79 +4,79 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('nome/usuario', function (Request $request) {
-    $nome = 'Malu';
+    $nome = $request->input('nome');
     return $nome;
 });
 
 Route::get('nome/idade', function (Request $request) {
-    $nome = 'Malu';
-    $idade = '16';
+    $nome = $request->input('nome');
+    $idade = $request->inpuit('idade');
     return "Meu nome é " . $nome . ", tenho" . $idade . " anos de idade";
 });
 
 Route::get('nome/cidade', function (Request $request) {
-    $nome = "Malu";
-    $ano = '2007';
-    $cidade = "Presidente Epitacio.";
+    $nome = $request->input('nome');
+    $ano = $request->input('ano');
+    $cidade = $request->input('cidade');
     return "Meu nome é " . $nome . ", nasci no ano de " . $ano . " na cidade de " . $cidade;
 });
 
 Route::get('somar', function (Request $request) {
-    $primeiroNumero = 10;
-    $segundoNumero = 20;
-    $resultado = $primeiroNumero * $segundoNumero;
+    $primeiroNumero = $request->input('primeiroNumero');
+    $segundoNumero = $request->input('segundoNumero');
+    $resultado = $primeiroNumero + $segundoNumero;
     return 'O resultado da soma dos numeros é: ' .
         $primeiroNumero . ' + ' . $segundoNumero . ' = ' . $resultado;
 });
 
 Route::get('subtrair', function (Request $request) {
-    $primeiroNumero = 60;
-    $segundoNumero = 20;
-    $terceiroNumero = 40;
+    $primeiroNumero = $request->input('primeiroNumero');
+    $segundoNumero = $request->input('segundoNumero');
+    $terceiroNumero = $request->input('terceiroNumero');
     $resultado = $primeiroNumero - $segundoNumero - $terceiroNumero;
     return 'O resultado da subtração dos numeros é: ';
     $primeiroNumero . ' - ' . $segundoNumero . ' - ' . $terceiroNumero . ' = ' . $resultado;
 });
 
 Route::get('dividir', function (Request $request) {
-    $primeiroNumero = 60;
-    $segundoNumero = 20;
+    $primeiroNumero = $request->input('primeiroNumero');
+    $segundoNumero = $request->input('segundoNumero');
     $resultado = $primeiroNumero / $segundoNumero;
     return 'O resultado da divisão é: ' .
         $primeiroNumero . ' / ' . $segundoNumero . ' = ' . $resultado;
 });
 
 Route::get('multiplicacao', function (Request $request) {
-    $primeiroNumero = 60;
-    $segundoNumero = 20;
+    $primeiroNumero = $request->input('primeiroNumero');
+    $segundoNumero = $request->input('segundoNumero');
     $resultado = $primeiroNumero * $segundoNumero;
     return 'O resultado da multiplicacao é: ' .
         $primeiroNumero . ' x ' . $segundoNumero . ' = ' . $resultado;
 });
 
 Route::get('media', function (Request $request) {
-    $primeiroNota = 6;
-    $segundoNota = 8;
-    $terceiroNota = 5;
-    $quartoNota = 9;
-    $quintoNota = 6;
+    $primeiroNota = $request->input('primeiroNota');
+    $segundoNota = $request->input('segundaNota');
+    $terceiroNota = $request->input('terceiraNota');
+    $quartoNota = $request->input('quartaNota');
+    $quintoNota = $request->input('quintaNota');
     $resultado = $primeiroNota + $segundoNota + $terceiroNota + $quartoNota + $quintoNota;
     return 'O resultado da media é: ' .
         $primeiroNota . ' + ' . $segundoNota . ' + ' . $terceiroNota . ' + ' . $quartoNota . ' + ' .  $quintoNota  . ' = ' . $resultado / 5;
 });
 
 Route::get('divisao', function (Request $request) {
-    $primeiroNumero = 30;
-    $segundoNumero = 20;
+    $primeiroNumero = $request->input('primeiroNumero');
+    $segundoNumero = $request->input('segundoNumero');
     $resultado = $segundoNumero / $primeiroNumero;
     return 'O resultado da divisão é: ' .
         $segundoNumero . ' / ' . $primeiroNumero . ' = ' . $resultado;
 });
 
 Route::get('dobro', function (Request $request) {
-    $primeiroNumero = 30;
+    $primeiroNumero = $request->input('primeiroNumero');
     $resultado = $primeiroNumero * 2;
-    return 'O dobro de 30 é igual a ' .
+    return 'O dobro é igual a ' .
         $resultado;
 });
 
